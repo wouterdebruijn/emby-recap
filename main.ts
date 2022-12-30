8b96289662f419377e66fa9dd2cd70cdfcb0fcb1
Wouter de Bruijn <wouter@hedium.nl>
2022-12-30 21:24:26 +0100

    🎉 Initialized fresh

---
+++ main.ts
@@ -0,0 +1,15 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import "https://deno.land/std@0.170.0/dotenv/load.ts";
+
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.ts";
+
+await start(manifest, { plugins: [twindPlugin(twindConfig)] });
