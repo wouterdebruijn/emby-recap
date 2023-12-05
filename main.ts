748efc7870d324b82fc21a296eaa370c908c8b90
Wouter de Bruijn <wouter@hedium.nl>
2023-12-05 16:15:03 +0100

    🚚 Moved plugins to fresh config

--- main.ts
+++ main.ts
@@ -1,13 +1,5 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="dom" />
-/// <reference lib="dom.iterable" />
-/// <reference lib="dom.asynciterable" />
-/// <reference lib="deno.ns" />
-
-import "https://deno.land/std@0.170.0/dotenv/load.ts";
-import tailwind from "$fresh/plugins/tailwind.ts";
-
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
 
-await start(manifest, { plugins: [tailwind()] });
+await start(manifest, config);
