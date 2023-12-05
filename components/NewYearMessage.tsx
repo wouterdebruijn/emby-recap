b01520c2b34d5447d8573bf02333a5685962a11d
Wouter de Bruijn <wouter@hedium.nl>
2023-12-05 17:52:14 +0100

    ♻️ Moved wrapping element into component

--- components/NewYearMessage.tsx
+++ components/NewYearMessage.tsx
@@ -1,10 +1,22 @@
 export function NewYearMessage() {
   const now = new Date();
+
+  let element = null;
+
   if (now.getMonth() === 11 && now.getDate() > 25) {
-    return <h1 class="text-6xl">Happy {now.getFullYear() + 1}!</h1>;
+    element = <h1 class="text-6xl">Happy {now.getFullYear() + 1}!</h1>;
   }
+
   if (now.getMonth() === 0 && now.getDate() < 5) {
-    return <h1 class="text-6xl">Happy {now.getFullYear()}</h1>;
+    element = <h1 class="text-6xl">Happy {now.getFullYear()}</h1>;
   }
-  return <></>;
+
+  return (
+    element &&
+    (
+      <section class="text-center font-thin pt-16">
+        {element}
+      </section>
+    )
+  );
 }
